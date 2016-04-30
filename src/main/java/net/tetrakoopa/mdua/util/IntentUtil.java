115064ec5f62e8b1fa32ee181bf40ba0cb7a9bb4
Martin Salazard <martin.der.aze@gmail.com>
2016-04-30 18:03:31 +0200

    Add IntentUtil

---
+++ src/main/java/net/tetrakoopa/mdua/util/IntentUtil.java
@@ -0,0 +1,44 @@
+package net.tetrakoopa.mdua.util;
+
+import android.content.Context;
+import android.content.Intent;
+
+import android.R;
+import android.net.Uri;
+import android.os.Parcelable;
+
+public class IntentUtil {
+
+	public static void shareText(Context context, int title, String subject, String content) {
+		shareText(context, context.getResources().getString(title), subject, content);
+	}
+	public static void shareText(Context context, String title, String subject, String content) {
+		final Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+		sharingIntent.setType("text/plain");
+		if (subject!=null)
+			sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, subject);
+		sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, content);
+		context.startActivity(Intent.createChooser(sharingIntent, title));
+	}
+
+	/**
+	 * Copy action and type from <code>source</code> intent to <code>destination</code> intent.<br/>
+	 * Also copy all extra parameters listed in <code>extras</code>
+	 */
+	public static void mimicIntent(Intent source, Intent destination, String... extras) {
+		final String action = source.getAction();
+		final String type = source.getType();
+		if (action!=null)
+			destination.setAction(action);
+		if (type!=null)
+			destination.setType(type);
+
+		for (String extra : extras) {
+			final Parcelable extraValue = source.getParcelableExtra(extra);
+			if (extraValue!=null)
+				destination.putExtra(extra, extraValue);
+		}
+
+	}
+
+}
